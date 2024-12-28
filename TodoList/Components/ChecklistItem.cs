5901f39766d737a9a47eb19dfeef577ea340dda1
Kalle Jönsson <ayo_lappskojs@msn.com>
2024-12-28 14:41:21 +0100

    Add project files.

---
+++ TodoList/Components/ChecklistItem.cs
@@ -0,0 +1,14 @@
+﻿namespace TodoList.Components
+{
+    public class ChecklistItem
+    {
+        public bool IsChecked { get; set; } = false;
+        public string Text { get; set; } = string.Empty;
+        public string Assignee { get; set; } = string.Empty;
+
+        public ChecklistItem(string assignee)
+        {
+            Assignee = assignee;
+        }
+    }
+}
